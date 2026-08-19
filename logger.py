46c2cdfd19f2c9da88313a7b9989ec86d0bfaf1c
Matteo Ricci <ariunahsmile9@outlook.com>
2026-08-19 17:15:23 +0200

    📝 docs:logging utility for crypto application

--- logger.py
+++ logger.py
@@ -1,31 +1,28 @@
 import logging
 
-class CustomLogger:
-    def __init__(self, name):
+class CryptoLogger:
+    def __init__(self, name='CryptoLogger'):
         self.logger = logging.getLogger(name)
         self.logger.setLevel(logging.DEBUG)
+        handler = logging.FileHandler('crypto.log')
+        handler.setLevel(logging.DEBUG)
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        ch = logging.StreamHandler()
-        ch.setFormatter(formatter)
-        self.logger.addHandler(ch)
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
 
     def debug(self, message):
-        self._log_with_error_handling(self.logger.debug, message)
+        self.logger.debug(message)
 
     def info(self, message):
-        self._log_with_error_handling(self.logger.info, message)
+        self.logger.info(message)
 
     def warning(self, message):
-        self._log_with_error_handling(self.logger.warning, message)
+        self.logger.warning(message)
 
     def error(self, message):
-        self._log_with_error_handling(self.logger.error, message)
+        self.logger.error(message)
 
     def critical(self, message):
-        self._log_with_error_handling(self.logger.critical, message)
+        self.logger.critical(message)
 
-    def _log_with_error_handling(self, log_method, message):
-        try:
-            log_method(message)
-        except Exception as e:
-            self.logger.error(f'Logging failed: {e}')
+crypto_logger = CryptoLogger()
