dec5781aa1b2d569fa5671f296b812bf7058b398
Matteo Ricci <ariunahsmile9@outlook.com>
2026-09-12 17:20:20 +0200

    test:rotational logging setup for crypto operations

--- logger.py
+++ logger.py
@@ -2,34 +2,36 @@ import logging
 from logging.handlers import RotatingFileHandler
 import os
 
-def get_crypto_logger(name='automation-tool-56'):
+def get_crypto_logger(name: str = 'automation-tool-56'):
     logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
+    logger.setLevel(logging.DEBUG)
     
-    if logger.hasHandlers():
-        logger.handlers.clear()
-
     formatter = logging.Formatter(
-        '[%(asctime)s] [%(levelname)s] [%(process)d] %(message)s',
+        '[%(asctime)s] [%(levelname)s] [%(filename)s:%(lineno)d] - %(message)s',
         datefmt='%Y-%m-%d %H:%M:%S'
     )
 
-    log_path = os.path.join(os.getcwd(), 'logs', 'crypto_engine.log')
-    os.makedirs(os.path.dirname(log_path), exist_ok=True)
-
-    handler = RotatingFileHandler(
+    log_dir = 'logs'
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+    
+    log_path = os.path.join(log_dir, 'crypto_ops.log')
+    
+    file_handler = RotatingFileHandler(
         log_path,
-        maxBytes=5 * 1024 * 1024,
-        backupCount=3,
+        maxBytes=10 * 1024 * 1024,
+        backupCount=5,
         encoding='utf-8'
     )
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-
-    console = logging.StreamHandler()
-    console.setFormatter(formatter)
-    logger.addHandler(console)
-
+    file_handler.setFormatter(formatter)
+    
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    
+    if not logger.handlers:
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
+        
     return logger
 
 logger = get_crypto_logger()
